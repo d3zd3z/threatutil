8ba555f6d9a4d2d55cd8b319290c0896a57b3759
David Brown <david.brown@linaro.org>
2017-12-28 13:41:07 -0700

    Update threats for the sensor
    The utility just dumps them out

--- main.go
+++ main.go
@@ -8,14 +8,44 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os/user"
 
 	"github.com/andygrunwald/go-jira"
 	"github.com/bgentry/go-netrc/netrc"
+	"github.com/kr/text"
+	"gopkg.in/yaml.v2"
 )
 
 func main() {
+	if false {
+		output()
+	} else {
+		input()
+	}
+}
+
+func input() {
+	// Try reading the yaml of the threats.
+	var threats map[string]*Threat
+
+	buf, err := ioutil.ReadFile("sensor.yaml")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = yaml.Unmarshal(buf, &threats)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for id, threat := range threats {
+		fmt.Printf("%s: %#v\n", id, threat)
+	}
+}
+
+func output() {
 	err := queryJira("davidb.atlassian.net", "project = THREAT")
 	if err != nil {
 		log.Fatal(err)
@@ -58,14 +88,63 @@ func queryJira(host string, query string) error {
 		start += len(issues)
 	}
 
+	// Reverse the results, which seem to be in reverse order of
+	// creation.
+	for i, j := 0, len(result)-1; i < j; i, j = i+1, j-1 {
+		result[i], result[j] = result[j], result[i]
+	}
+
+	// enc := toml.NewEncoder(os.Stdout)
+	threats := make(map[string]*Threat)
+
 	for _, issue := range result {
-		log.Printf("issue: %+v", issue.ID)
-		for k, v := range issue.Fields.Unknowns {
-			log.Printf("  %s: %+v", k, v)
+		threat := Threat{
+			Summary: issue.Fields.Summary,
+			Desc:    issue.Fields.Description,
+			Resp:    unknownGet(issue.Fields.Unknowns, "customfield_10203"),
+			Sec:     unknownGet(issue.Fields.Unknowns, "customfield_10202"),
+			Imp:     unknownGet(issue.Fields.Unknowns, "customfield_10201"),
 		}
+		threats[issue.Key] = &threat
+
+		fmt.Printf("%s:\n  summary: %s\n", issue.Key, threat.Summary)
+		show("desc", threat.Desc)
+		show("resp", threat.Resp)
+		show("sec", threat.Sec)
+		show("imp", threat.Imp)
 	}
 
 	return nil
+}
+
+func show(key, line string) {
+	wrapped := text.Wrap(line, 65)
+	indented := text.Indent(wrapped, "    ")
+	fmt.Printf("  %s: >-\n%s\n", key, indented)
+}
+
+func unknownGet(fields map[string]interface{}, key string) string {
+	item, ok := fields[key]
+	if !ok {
+		return ""
+	}
+
+	switch it := item.(type) {
+	case string:
+		return it
+	case nil:
+		return ""
+	default:
+		panic("Unknown type")
+	}
+}
+
+type Threat struct {
+	Summary string
+	Desc    string
+	Resp    string
+	Sec     string
+	Imp     string
 }
 
 type Login struct {
